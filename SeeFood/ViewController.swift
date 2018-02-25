47f0b4157679a6e53be6282762d4228eca7d9ef5
Your Name <oz9080@gmail.com>
2018-02-25 15:46:23 +0200

    Add Camera Functionality and InceptionV3 ML Model.

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker : UIImagePickerController = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+        } else {
+            let alertController = UIAlertController.init(title: "Error", message: "No Camera", preferredStyle: .alert)
+            
+            let alertAction = UIAlertAction.init(title: "Fanan", style: .default, handler: nil)
+            
+            alertController.addAction(alertAction)
+            self.present(alertController, animated: true, completion: nil)
+            
+            imagePicker.sourceType = .savedPhotosAlbum
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            imageView.image = userPickedImage
+            
+            imagePicker.dismiss(animated: true, completion: nil)
+        }
     }
-
-
+    
+    
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
 }
 
